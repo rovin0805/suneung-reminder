3585e45747bdf0a3b9340e5ce8c7884b30ffe2ea
Choi Eun Jeong <qhdrn67@naver.com>
2024-11-07 23:16:01 +0900

    style: tawilwind color + global style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "./src/styles/colors";
 
 const config: Config = {
   content: [
@@ -8,10 +9,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors,
     },
   },
   plugins: [],
