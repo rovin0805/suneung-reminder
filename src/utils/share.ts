64dec1608339b6d3134ec84850ebeaa2666c6956
Choi Eun Jeong <qhdrn67@naver.com>
2024-11-10 22:59:37 +0900

    fix: 4. 안드로이드 (카톡으로 사이트 열었을 때) > 상단 우측 공유하기 상단, 결과창 내 공유하기 미작동

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -2,10 +2,24 @@ import { WEBSITE_URL } from "@/constants";
 
 export const share = async () => {
   if (navigator.share) {
-    await navigator.share({
-      title: "눈 떠보니 수능날? | afterdinnerclub",
-      text: "내가 봤던 수능 다시 보기!",
-      url: WEBSITE_URL,
-    });
+    try {
+      await navigator.share({
+        title: "눈 떠보니 수능날? | afterdinnerclub",
+        text: "내가 봤던 수능 다시 보기!",
+        url: WEBSITE_URL,
+      });
+    } catch {
+      await navigator.clipboard.writeText(WEBSITE_URL);
+    }
+  } else if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(WEBSITE_URL);
+    } catch {
+      alert(
+        "이런! 공유하는 중 문제가 발생했어요. 수동으로 URL을 복사해주세요."
+      );
+    }
+  } else {
+    alert("이런! 공유하는 중 문제가 발생했어요. 수동으로 URL을 복사해주세요.");
   }
 };
