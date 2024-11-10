97a67012e4f218eb8ca3f4e52fea0c9bf4de57ef
Choi Eun Jeong <qhdrn67@naver.com>
2024-11-10 22:21:44 +0900

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import ColorContainer from "@/components/layout/ColorContainer";
